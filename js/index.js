bbafdd8348961d2ab304856872d90af0b6be2304
isaac <isaac.ingles@gmail.com>
2020-04-23 13:23:43 -0600

    started messing around with animations

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Custom JS here
+
+// Header animations
+gsap.from(".navbar-brand", { duration: 5, x: 50 });
+gsap.from("#navbarNavDropdown", { duration: 5, x: 50 });
+
+
+// Hero h1 animation
+gsap.from(".fadeIn", {
+    duration: 1,
+    opacity: 0
+})
+
+// the animation to use
+const tl = gsap.timeline({ paused: true });
+tl.from(".scroll", { scale: 0.7, autoAlpha: 0 });
+
+// The start and end positions in terms of the page scroll
+const startY = innerHeight / 10;
+const finishDistance = innerHeight / 5;
+
+// Listen to the scroll event
+document.addEventListener("scroll", function () {
+    // Prevent the update from happening too often (throttle the scroll event)
+    if (!requestId) {
+        requestId = requestAnimationFrame(update);
+    }
+});
+
+update();
+
+function update() {
+    // Update our animation
+    tl.progress((scrollY - startY) / finishDistance);
+
+    // Let the scroll event fire again
+    requestId = null;
+}
--- readme.md
+++ readme.md
@@ -6,3 +6,6 @@
 
 - Original Site Layout taken from [Grand America Hotels](https://www.grandamerica.com/)
 - Site is hosted live at [Netlify](https://iingles-bootstrap.netlify.com/)
+
+
+- Animations done with GreenSock
